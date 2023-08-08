c9f96a2e4aefdaef45005907eae59351a2ee6e6c
陈佳穗 <344516330@qq.com>
2023-08-08 16:36:37 +0800

    feat: 增加markdown和封装edit

---
+++ components/common/common-md.vue
@@ -0,0 +1,140 @@
+<template>
+  <div class="markdown-box">
+    <div v-if="edit_able" class="button-box">
+      <el-button class="markdown-button" type="text" :disabled="editing&&!input_value"
+        @click="clickEdit">{{editing?'确认':'编辑'}}</el-button>
+    </div>
+    <div class="content-scroll">
+      <template v-if="editing||!input_value">
+        <el-input v-model="input_value" class="markdown-input" type="textarea" :autosize="{ minRows: 5}"></el-input>
+      </template>
+      <template v-else>
+        <div v-show="!editing&&markdown_content" v-html="markdown_content" class="markdown-container"></div>
+      </template>
+    </div>
+  </div>
+</template>
+
+<script>
+module.exports = {
+  name: 'common-md',
+  props: {
+    edit_able: {
+      type: Boolean,
+      default: () => true
+    },
+    value: {
+      type: String,
+      default: () => ''
+    },
+    default_status: {
+      type: Boolean,
+      default: () => false
+    },
+  },
+  data() {
+    return {
+      input_value: '',
+      markdown_content: '',
+      editing: false,
+      md: null
+    }
+  },
+  watch: {
+    value: {
+      handler(val) {
+        this.input_value = this.value
+      },
+      deep: true
+    },
+
+  },
+  created() {
+    this.input_value = this.value
+    this.editing = this.default_status
+    if (!this.editing && this.input_value) {
+      this.$nextTick()
+        .then(() => {
+          this.markdown_content = this.md.render(this.input_value)
+          this.renderMermaid()
+        })
+    }
+  },
+  mounted() {
+    this.initializeMarkdown()
+  },
+  methods: {
+    clickEdit() {
+      this.editing = !this.editing
+      if (!this.editing) {
+        this.markdown_content = this.md.render(this.input_value)
+        this.renderMermaid()
+        this.$emit('input', this.input_value)
+        this.$emit('change', this.input_value)
+      }
+    },
+    initializeMarkdown() {
+      let defaults = {
+        xhtmlOut: true,
+        breaks: false,
+        langPrefix: 'language-',
+        html: true,
+        linkify: true,
+        typographer: true,
+      }
+
+      defaults.highlight = (str, lang) => {
+        var esc = this.md.utils.escapeHtml
+        if (lang && hljs.getLanguage(lang)) {
+          try {
+            return '<pre class="hljs"><code>' +
+              hljs.highlight(lang, str, true).value +
+              '</code></pre>'
+          } catch (__) { }
+        } else if (lang == 'mermaid') {
+          return `<div class="mermaid">${str}</div>`
+        } else {
+          return '<pre class="hljs"><code>' + esc(str) + '</code></pre>'
+        }
+      }
+      this.md = window.markdownit(defaults)
+      this.md.use(window.markdownitTaskLists, { enable: true })
+      this.md.validateLink = function () { return true }
+    },
+    renderMermaid() {
+      this.$nextTick(() => {
+        mermaid.run({
+          querySelector: `.mermaid`,
+        })
+      })
+    }
+  },
+};
+</script>
+
+<style scoped>
+.markdown-box {
+  display: flex;
+  overflow: hidden;
+  flex-direction: column;
+  width: 100%;
+  flex: 1;
+  text-align: left !important;
+}
+
+.content-scroll {
+  width: 100%;
+  flex: 1;
+  overflow: auto;
+}
+
+.markdown-input {
+  width: 100%;
+  height: 100%;
+}
+
+.markdown-container {
+  width: 100%;
+  height: 100%;
+}
+</style>
--- components/common/common-table.vue
+++ components/common/common-table.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="table-box">
     <div class="only-table">
-      <el-table class="class-table" stripe :border="true" :height="height" :data="data" :row-key="row_key"
-        v-bind="$attrs" v-on="$listeners">
-        <el-table-column v-if="selection" type="selection" width="55">
+      <el-table class="class-table" ref="common_table" stripe :border="true" :height="height" :data="data"
+        :row-key="row_key" v-bind="$attrs" v-on="$listeners" @select="handleSelectChange"
+        @select-all="handleSelectChangeAll" :tree-props="{children: 'children', hasChildren: 'hasChildren'}">
+        <el-table-column v-if="multiple_option && multiple_option.status" type="selection" width="55">
         </el-table-column>
         <template v-for="(prop, index) in props">
           <el-table-column :key="index" :prop="prop.prop || prop.key" :label="prop.label" :sortable="prop.sortable"
-            :width="prop.width" :min-width="prop.min_width" :align="prop.align || 'center'" :fixed="prop.fixed">
+            :width="prop.width" :min-width="prop.min_width" :align="prop.align || 'center'" :fixed="prop.fixed"
+            :type="prop.expand?'expand':''">
             <template v-if="prop.label_tooltip">
               <template slot="header">
                 <span class="flex aic jcc">
@@ -28,53 +30,24 @@
             </template>
             <template v-if="prop.key" slot-scope="scope">
               <slot v-if="prop.slot_name" :name="prop.slot_name" :row="scope.row" :index="scope.$index"></slot>
-              <template v-else-if="prop.filter">
-                {{ scope.row[prop.key] | filterValue(prop.filter, prop.filterParams && prop.filterParams.map(i => i.key &&
-                  scope.row[i.key] || i)) }}
-              </template>
               <template v-else-if="prop.edit_type">
-                <template v-if="prop.edit_type === 'select'">
-                  <el-select v-model="scope.row[prop.key]" @change="editItem({ key: prop.key, index: scope.$index })">
-                    <el-option v-for="(item, select_index) in prop.select_list" :key="select_index"
-                      :label="item[prop.label_key || 'label']" :value="item[prop.value_key || 'value']"></el-option>
-                  </el-select>
-                </template>
-                <template v-if="prop.edit_type === 'image_url'">
-                  <common-upload :url.sync="scope.row[prop.key]"
-                    @change="editItem({ key: prop.key, index: scope.$index })"></common-upload>
-                </template>
-                <template v-if="prop.edit_type === 'input'">
-                  <el-input v-model="scope.row[prop.key]" @blur="editItem({ key: prop.key, index: scope.$index })">
-                  </el-input>
-                </template>
-                <template v-if="prop.edit_type === 'textarea'">
-                  <el-input v-model="scope.row[prop.key]" type="textarea" :autosize="{ minRows: 1, maxRows: 2 }"
-                    @blur="editItem({ key: prop.key, index: scope.$index })"></el-input>
-                </template>
-                <template v-if="prop.edit_type === 'number'">
-                  <el-input v-model.number="scope.row[prop.key]"
-                    @blur="editItem({ key: prop.key, index: scope.$index })"></el-input>
-                </template>
-                <template v-if="prop.edit_type === 'boolean'">
-                  <el-switch v-model="scope.row[prop.key]" :active-text="prop.active_text || '是'"
-                    :inactive-text="prop.unactive_text || '否'"
-                    @change="editItem({ key: prop.key, index: scope.$index })">
-                  </el-switch>
-                </template>
+                <common-edit :row="scope.row" :prop="prop"
+                  @edit-item="({value,key,id})=>editItem({value,key,id,index:scope.$index,row:scope.row})" />
               </template>
               <template v-else>
                 <template v-if="prop.copytext">
                   <el-tooltip class="item" effect="dark" placement="top">
-                    <div slot="content" style="white-space: pre-line">
-                      {{scope.row[prop.key]}}
+                    <div slot="content" style="white-space: pre-line !important;max-height:60vh;overflow-y: scroll;">
+                      {{ scope.row[prop.key] | filterValue(prop.filter, prop.filterParams && prop.filterParams.map(i => (i.key && scope.row[i.key]) || i))  }}
                     </div>
-                    <div class="text-overflow3" v-copy-text="scope.row[prop.key]" style="max-width: 100%;">
-                      {{ scope.row[prop.key] || empty_val }}
+                    <div class="text-overflow3" v-copy-text style="max-width: 100%;">
+                      {{ scope.row[prop.key] | filterValue(prop.filter, prop.filterParams && prop.filterParams.map(i => (i.key && scope.row[i.key]) || i))  }}
                     </div>
                   </el-tooltip>
                 </template>
                 <template v-else>
-                  {{ scope.row[prop.key] || empty_val }}
+                  {{ scope.row[prop.key] | filterValue(prop.filter, prop.filterParams && prop.filterParams.map(i => (i.key &&
+                  scope.row[i.key]) || i))  }}
                 </template>
               </template>
             </template>
@@ -93,7 +66,7 @@ let vm
 module.exports = {
   components: {
     'common-pagination': 'url:./common-pagination.vue',
-    'common-upload': 'url:./common-upload.vue',
+    'common-edit': 'url:./common-edit.vue',
   },
   filters: {
     /* 过滤器方法,默认返回指定的值和空字符串 */
@@ -102,14 +75,10 @@ module.exports = {
       if (fun && vm[fun]) {
         return vm[fun](val, ...filterParams)
       }
-      return val || ''
+      return val || vm.empty_val
     },
   },
   props: {
-    table_data: {
-      type: Array,
-      default: () => []
-    },
     data: {
       type: Array,
       default: () => []
@@ -146,24 +115,196 @@ module.exports = {
       type: String,
       default: () => 'objectId'
     },
+    multiple_option: {
+      type: Object,
+      default: () => ({})
+    },
   },
   data() {
     vm = this
     return {
-
+      selected_keys: new Set(), // 所有选中的行的 key 集合
+      selected_list: [], // 所有选中的行的所有数据集合
+      delete_keys: new Set(), // 排除掉的行的 key
+    }
+  },
+  computed: {
+    expandData() {
+      return util.expandList(this.data)
+    }
+  },
+  watch: {
+    data(val) {
+      this.$nextTick(() => {
+        let seleced_rows = []
+        if (this.multiple_option.keep_alive && !this.multiple_option.auto_set) {
+          seleced_rows = this.data.filter(i => this.selected_keys.has(i[this.multiple_option.key]))
+        } else if (this.multiple_option.auto_set) {
+          if (this.multiple_option.keep_alive) {
+            seleced_rows = this.data.filter(i => i[this.multiple_option.autoKey] === this.multiple_option.autoValue && !this.delete_keys.has(i[this.multiple_option.key]))
+          } else {
+            seleced_rows = this.data.filter(i => this.selected_keys.has(i[this.multiple_option.key || 'objectId']))
+          }
+        }
+        this.toggleSelection(seleced_rows)
+        this.$refs.common_table.doLayout()
+      })
+    },
+    props: {
+      handler() {
+        setTimeout(() => {
+          this.$refs.common_table.doLayout()
+        })
+      },
+      deep: true
+    },
+    multiple_option: {
+      handler() {
+        this.$nextTick(() => {
+          let seleced_rows = []
+          if (this.multiple_option.auto_set) {
+            if (this.multiple_option.keep_alive) {
+              seleced_rows = this.data.filter(i => i[this.multiple_option.autoKey] === this.multiple_option.autoValue && !this.delete_keys.has(i[this.multiple_option.key]))
+            } else {
+              this.selected_keys = new Set(this.multiple_option.select_list.map(i => i[this.multiple_option.key || 'objectId']))
+              seleced_rows = this.data.filter(i => this.selected_keys.has(i[this.multiple_option.key || 'objectId']))
+            }
+          }
+          this.toggleSelection(seleced_rows)
+          this.$refs.common_table.doLayout()
+        })
+      },
+      deep: true,
+      immediate: true,
     }
   },
   methods: {
     pageChange(obj) {
       this.$emit('page-change', obj)
     },
+    // 单选
+    handleSelectChange(val) {
+      let idList = val.map(i => i[this.multiple_option.key || 'objectId'])
+      for (let item of this.data) {
+        let id_list_have = idList.includes(item[this.multiple_option.key || 'objectId'])
+        let select_have = this.selected_keys.has(item[this.multiple_option.key || 'objectId'])
+        if (id_list_have && !select_have) { //
+          this.selected_keys.add(item[this.multiple_option.key || 'objectId'])
+          this.selected_list.push(item)
+          this.delete_keys.delete(item[this.multiple_option.key || 'objectId'])
+        } else if (!id_list_have && select_have) {
+          this.selected_keys.delete(item[this.multiple_option.key || 'objectId'])
+          let idx = this.selected_list.findIndex(i => i === item[this.multiple_option.key || 'objectId'])
+          this.selected_list.splice(idx, 1)
+          this.delete_keys.add(item[this.multiple_option.key || 'objectId'])
+        }
+      }
+      if (this.multiple_option.select_change === 'key') {
+        this.$emit('selection-change', this.selected_keys)
+      } else {
+        this.$emit('selection-change', this.selected_list)
+      }
+    },
+    // 全选
+    handleSelectChangeAll(val) {
+      if (val.length) {
+        for (let item of this.data) {
+          if (!this.selected_keys.has(item[this.multiple_option.key || 'objectId'])) {
+            this.selected_keys.add(item[this.multiple_option.key || 'objectId'])
+            this.selected_list.push(item)
+            this.delete_keys.delete(item[this.multiple_option.key || 'objectId'])
+          }
+        }
+      } else {
+        for (let item of this.data) {
+          if (this.selected_keys.has(item[this.multiple_option.key || 'objectId'])) {
+            this.selected_keys.delete(item[this.multiple_option.key || 'objectId'])
+            let idx = this.selected_list.findIndex(i => i === item[this.multiple_option.key || 'objectId'])
+            this.selected_list.splice(idx, 1)
+            this.delete_keys.add(item[this.multiple_option.key || 'objectId'])
+          }
+        }
+      }
+      if (this.multiple_option.select_change === 'key') {
+        this.$emit('selection-change', this.selected_keys)
+      } else {
+        this.$emit('selection-change', this.selected_list)
+      }
+    },
+    // 设置选中 会触发change事件
+    toggleSelection(rows) {
+      if (rows && rows.length) {
+        rows.forEach((row) => {
+          this.$refs.common_table.toggleRowSelection(row, true)
+        })
+      } else {
+        this.$refs.common_table.clearSelection()
+      }
+    },
+    // 重置选中的内容
+    selectRest() {
+      this.selected_keys = []
+      this.selected_list = []
+      if (this.multiple_option.select_change === 'key') {
+        this.$emit('selection-change', this.selected_keys)
+      } else {
+        this.$emit('selection-change', this.selected_list)
+      }
+    },
+    // 删除一个数据，通过 multiple_option.condition_key 对应的值
+    deleteSelectInValue(val) {
+      let idx = this.selected_keys.findIndex(i => i === val)
+      if (idx > -1) {
+        this.selected_keys.delete(item[this.multiple_option.key || 'objectId'])
+        this.selected_list.splice(idx, 1)
+        if (this.multiple_option.select_change === 'key') {
+          this.$emit('selection-change', this.selected_keys)
+        } else {
+          this.$emit('selection-change', this.selected_list)
+        }
+      }
+    },
+    editItem({ key = '', index = '', value = '', id = '', row }) {
+      let item = this.expandData.find(i => i[this.row_key] === id)
+      let edit_value = value || item[key] || this.data[index][key]
+      this.$emit('edit-item', { value: edit_value, key, index, id, row })
+    },
+    getRelation({ row = {}, index = 0, query = '', key = '', object_class = '', query_key = 'name' } = {}) {
+      if (!row) return
+      row.loading = true
+      lc.readTotal(object_class, (q) => {
+        q.contains(query_key, query)
+      })
+        .then(res => {
+          let { data, count } = res
+          row[`${key}_list`] = data.map(i => i.toJSON())
+          row.loading = false
+        })
+        .catch(err => {
+          console.log(err)
+          this.$alert(err)
+          row.loading = false
+        })
+    },
+    changeRelation({ index = 0, value, key = '', object_class = '' } = {}) {
+      let lc_object = lc.createObject(object_class, value)
+      this.editItem({ value: lc_object, key, index })
+    },
     // filter方法
     formateTime(val, format = 'YYYY/MM/DD HH:mm') {
       return dayjs(val).format(format)
     },
-    editItem({ key = '', index = '' }) {
-      let value = this.data[index][key]
-      this.$emit('edit-item', { value, key, index })
+    formateStringNumberTime(val, format = 'YYYY/MM/DD HH:mm') {
+      return dayjs(+val).format(format)
+    },
+    formateListLabel(val, list, value_key = 'value', label_key = 'label') {
+      if (!list.length) return val
+      let item = list.find(i => i[value_key] === val)
+      if (item) return item[label_key]
+      return val
+    },
+    formatObject(val) {
+      return JSON.stringify(val || {}, null, 2)
     }
   },
 }
